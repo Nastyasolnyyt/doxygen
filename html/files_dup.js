3bca8e953a07860d0321bb9a2a7a10f5ea6167b8
Nastyasolnyyt <116307870+Nastyasolnyyt@users.noreply.github.com>
2025-05-07 14:01:41 +0300

    Add files via upload

---
+++ html/files_dup.js
@@ -0,0 +1,15 @@
+var files_dup =
+[
+    [ "auth_form.h", "auth__form_8h_source.html", null ],
+    [ "client.h", "client_8h_source.html", null ],
+    [ "client_main_window.h", "client__main__window_8h_source.html", null ],
+    [ "client_object.h", "client__object_8h_source.html", null ],
+    [ "clients_func.h", "clients__func_8h_source.html", null ],
+    [ "dbsingleton.h", "dbsingleton_8h_source.html", null ],
+    [ "functions_for_server.h", "functions__for__server_8h_source.html", null ],
+    [ "mytcpserver.h", "mytcpserver_8h_source.html", null ],
+    [ "notification.h", "notification_8h_source.html", null ],
+    [ "reg_form.cpp", "reg__form_8cpp.html", null ],
+    [ "reg_form.h", "reg__form_8h_source.html", null ],
+    [ "reset_password.h", "reset__password_8h_source.html", null ]
+];
---
+++ html/hierarchy.js
@@ -0,0 +1,21 @@
+var hierarchy =
+[
+    [ "clients_func", "classclients__func.html", null ],
+    [ "DBSingletonDestroyer", "class_d_b_singleton_destroyer.html", null ],
+    [ "MyTcpServerDestroyer", "class_my_tcp_server_destroyer.html", null ],
+    [ "QObject", null, [
+      [ "Client", "class_client.html", null ],
+      [ "DBSingleton", "class_d_b_singleton.html", null ],
+      [ "MyTcpServer", "class_my_tcp_server.html", null ],
+      [ "client", "classclient.html", null ],
+      [ "functions_for_server", "classfunctions__for__server.html", null ]
+    ] ],
+    [ "QWidget", null, [
+      [ "Widget", "class_widget.html", null ],
+      [ "auth_form", "classauth__form.html", null ],
+      [ "client_main_window", "classclient__main__window.html", null ],
+      [ "notification", "classnotification.html", null ],
+      [ "reset_password", "classreset__password.html", null ]
+    ] ],
+    [ "SingletonDestroyer", "class_singleton_destroyer.html", null ]
+];
